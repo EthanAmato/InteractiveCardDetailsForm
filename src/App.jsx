8c967110c0f8d7db6d60ccc630c3af7c55d13feb
EthanAmato <ethannamato@gmail.com>
2023-02-16 11:12:36 -0500

    added responsive credit cards according to template and started form

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import CreditCard from './components/CreditCard'
 import './styles/formStyles.scss'
 function App() {
 
   return (
     <main>
+      <div className='creditCardContainer'>
+        <CreditCard />
+      </div>
+      <div className='formContainer'>
+        <form>
 
-      0000 0000 0000 0000
-      Jane Appleseed
-      00/00
-
-      000
-
-      Cardholder Name
-      e.g. Jane Appleseed
-
-      Card Number
-      e.g. 1234 5678 9123 0000
-
-      Exp. Date (MM/YY)
-      MM
-      YY
-
-      CVC
-      e.g. 123
-
-      Confirm
-
-      {/* <!-- Completed state start --> */}
+          <div className='input full'>
+            <label htmlFor="name">Cardholder Name</label>
+            <input name='name' id='name' />
+          </div>
+          <div className='input full'>
+            <label htmlFor="name">Card Number</label>
+            <input name='cardNumber' id='cardNumber' />
+          </div>
+          <div className='input group'>
+            <label htmlFor="name">EXP. DATE (MM/YY)</label>
+            <input name='cardNumber' id='cardNumber' />
+            <input name='cardNumber' id='cardNumber' />
+            <label htmlFor="name">CVC</label>
+            <input name='cardNumber' id='cardNumber' />
+          </div>
+        </form>
+        {/* <!-- Completed state start -->
 
       Thank you!
       We've added your card details
-      Continue
+      Continue */}
+      </div>
     </main>
   )
 }
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,8 +1,25 @@
-function CreditCard({cardNumber, cardSecurityCode}) {
+import { useRef } from 'react';
+import '../styles/creditCardStyles.scss'
 
-    return(
-        <div>
-            Hi
+function CreditCard({ cardNumber, cardSecurityCode }) {
+    const cardNumberRef = useRef()
+    const cardSecurityRef = useRef()
+    return (
+        <div className='creditCardWrapper'>
+            <div className="frontSide">
+                <div className='shapes'>
+                    <div className='circle-1'></div>
+                    <div className='circle-2'></div>
+                </div>
+                <p className='cardNumber'>0000 0000 0000 0000</p>
+                <p className='name'>Jane Appleseed</p>
+                <p className='expirationDate'>00/00</p>
+            </div>
+            <div className="backSide">
+                <p className='securityCode'>000</p>
+            </div>
         </div>
     )
 }
+
+export default CreditCard;

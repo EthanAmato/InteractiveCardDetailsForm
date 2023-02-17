ad2d653a8a2806f75998a10df3f7e19cc72bd7b7
EthanAmato <ethannamato@gmail.com>
2023-02-17 13:47:08 -0500

    relocated some cc styles, fixed responsiveness issues, fixed overflow issues, light styling on form

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,31 @@ function App() {
 
   return (
     <main>
-      <div className='creditCardContainer'>
-        <CreditCard />
-      </div>
+      <CreditCard />
       <div className='formContainer'>
         <form>
 
           <div className='input full'>
             <label htmlFor="name">Cardholder Name</label>
-            <input name='name' id='name' />
+            <input type="text" name='name' id='name' />
           </div>
           <div className='input full'>
             <label htmlFor="name">Card Number</label>
-            <input name='cardNumber' id='cardNumber' />
+            <input type="text" name='cardNumber' id='cardNumber' />
           </div>
           <div className='input group'>
             <label htmlFor="name">EXP. DATE (MM/YY)</label>
-            <input name='cardNumber' id='cardNumber' />
-            <input name='cardNumber' id='cardNumber' />
+            <input type="text" name='cardNumber' id='cardNumber' />
+            <input type="text" name='cardNumber' id='cardNumber' />
             <label htmlFor="name">CVC</label>
-            <input name='cardNumber' id='cardNumber' />
+            <input type="text" name='cardNumber' id='cardNumber' />
           </div>
         </form>
         {/* <!-- Completed state start -->
 
-      Thank you!
-      We've added your card details
-      Continue */}
+          Thank you!
+          We've added your card details
+          Continue */}
       </div>
     </main>
   )
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -5,18 +5,20 @@ function CreditCard({ cardNumber, cardSecurityCode }) {
     const cardNumberRef = useRef()
     const cardSecurityRef = useRef()
     return (
-        <div className='creditCardWrapper'>
-            <div className="frontSide">
-                <div className='shapes'>
-                    <div className='circle-1'></div>
-                    <div className='circle-2'></div>
+        <div className='creditCardContainer'>
+            <div className='creditCardWrapper'>
+                <div className="frontSide">
+                    <div className='shapes'>
+                        <div className='circle-1'></div>
+                        <div className='circle-2'></div>
+                    </div>
+                    <p className='cardNumber'>0000 0000 0000 0000</p>
+                    <p className='name'>Jane Appleseed</p>
+                    <p className='expirationDate'>00/00</p>
                 </div>
-                <p className='cardNumber'>0000 0000 0000 0000</p>
-                <p className='name'>Jane Appleseed</p>
-                <p className='expirationDate'>00/00</p>
-            </div>
-            <div className="backSide">
-                <p className='securityCode'>000</p>
+                <div className="backSide">
+                    <p className='securityCode'>000</p>
+                </div>
             </div>
         </div>
     )

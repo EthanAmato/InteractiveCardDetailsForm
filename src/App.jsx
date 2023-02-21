e7e2c6c3ede3af2eb2ff66094a6292c8467beff5
EthanAmato <ethannamato@gmail.com>
2023-02-21 11:21:26 -0500

    added validation, state passing, and some logic for interactive card number effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import CreditCard from './components/CreditCard'
 import './styles/formStyles.scss'
 function App() {
   const [error, setError] = useState(false);
 
+  const [cardNumber, setCardNumber] = useState();
+
+  const nameRef = useRef();
+  const numberRef = useRef();
+  const dateRef = useRef();
+  const codeRef = useRef();
 
   function ensureNumDigits(e, numDigits) {
     if (e.target.value.length > numDigits) {
@@ -13,31 +19,51 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    setError(true);
-    console.log(e.target['name'].value)
-    console.log(e.target['cardNumber'].value)
-    console.log(e.target['name'])
-    console.log(e.target['name'])
+
+    let cardName = nameRef.current.value;
+    let cardNumber = numberRef.current.value;
+    let expMonth = parseInt(dateRef.current.children['expMonth'].value);
+    let expYear = parseInt(dateRef.current.children['expYear'].value);
+    let cvc = codeRef.current.value;
+
+    let currentYear = parseInt(String(new Date().getFullYear()).slice(2,)) //take last two digits
+    let currentMonth = parseInt(new Date().getMonth() + 1) //month is 0-indexed and clients will not be putting in 0-indexed dates
+
+    
+    if(!cardName || !cardNumber || !expMonth || !expYear || !cvc) setError(true)
+    else if (cardNumber.length < 16) setError(true)
+    else if (expMonth>12 || expMonth <= 0) setError(true)
+    else if (expYear < currentYear || (expYear === currentYear && expMonth > currentMonth)) setError(true)
+    else {
+      console.log("Congrats")
+    }
   }
 
   return (
     <main>
-      <CreditCard />
+      <CreditCard cardNumber={cardNumber} />
       <div className='formContainer'>
         <form onSubmit={handleSubmit}>
 
           <div className='input full'>
             <label htmlFor="name">Cardholder Name</label>
-            <input type="text" name='name' id='name' />
+            <input type="text" name='name' id='name' ref={nameRef}/>
             {error && <label className='error'>Can't leave name blank</label>}
           </div>
           <div className='input full'>
             <label htmlFor="cardNumber">Card Number</label>
-            <input type="number" name='cardNumber' id='cardNumber' maxLength={16} onInput={(e) => ensureNumDigits(e, 16)} />
+            <input type="number" 
+                    name='cardNumber' 
+                    id='cardNumber' 
+                    maxLength={16} 
+                    onInput={(e)=>ensureNumDigits(e, 16)} 
+                    onChange={(e)=> setCardNumber(() => e.target.value)} 
+                    ref={numberRef} 
+            />
             {error && <label className='error'>Can't leave card number blank</label>}
           </div>
           <div className='input group'>
-            <div>
+            <div ref={dateRef}>
               <label htmlFor="expMonth">EXP. DATE (MM/YY)</label><br />
               <input type="text" name='expMonth' id='expMonth' />
               <input type="text" name='expYear' id='expYear' /><br />
@@ -45,7 +71,7 @@ function App() {
             </div>
             <div>
               <label htmlFor="cvc">CVC</label><br />
-              <input type="number" name='cvc' id='cvc' onInput={(e) => ensureNumDigits(e, 3) }/><br />
+              <input type="number" name='cvc' id='cvc' onInput={(e) => ensureNumDigits(e, 3) } ref={codeRef}/><br />
               {error && <label className='error'>Must Fill Out CVC</label>}
             </div>
           </div>
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,9 +1,25 @@
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import '../styles/creditCardStyles.scss'
 
 function CreditCard({ cardNumber, cardSecurityCode }) {
-    const cardNumberRef = useRef()
-    const cardSecurityRef = useRef()
+    let templateString = "0000 0000 0000 0000"
+    const [parsedCardNumber, setParsedCardNumber] = useState("0000 0000 0000 0000")
+    useEffect(() => {
+        if (!cardNumber) return
+        setParsedCardNumber((current) => {
+            let temp = current[cardNumber.length - 1]
+            console.log()
+            if (temp !== "0") {
+                return cardNumber.slice(0, cardNumber.length) + " " + current.slice(cardNumber.length-1,current.length)
+            } else {
+                return cardNumber.slice(0, cardNumber.length) + current.slice(cardNumber.length-1,current.length)
+            }
+            return current
+        })
+
+    }, [cardNumber])
+    
+
     return (
         <div className='creditCardContainer'>
             <div className='creditCardWrapper'>
@@ -12,7 +28,7 @@ function CreditCard({ cardNumber, cardSecurityCode }) {
                         <div className='circle-1'></div>
                         <div className='circle-2'></div>
                     </div>
-                    <p className='cardNumber'>0000 0000 0000 0000</p>
+                    <p className='cardNumber'>{parsedCardNumber||templateString}</p>
                     <p className='name'>Jane Appleseed</p>
                     <p className='expirationDate'>00/00</p>
                 </div>
